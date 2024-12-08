26f3a6135f9f6e485ad584eafc30f13ca40943aa
MakeITSimple7 <vinhhoangngoc22@gmail.com>
2024-12-08 11:19:42 +0700

    Handle error with axios interceptor and toastify

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,15 +1,31 @@
-import axios, {type AxiosInstance} from 'axios'
+import axios, { type AxiosInstance, HttpStatusCode } from 'axios'
+import { toast } from 'react-toastify';
 
 class Http {
   instance: AxiosInstance
+
   constructor() {
     this.instance = axios.create({
       baseURL: 'https://api-ecom.duthanhduoc.com/',
       timeout: 10000,
-      headers:{
+      headers: {
         'Content-Type': 'application/json'
       }
     })
+
+    // Add a response interceptor
+    this.instance.interceptors.response.use(
+      function(response: any) {
+        return response
+      }, function(error: any) {
+        if (error.response.status != HttpStatusCode.UnprocessableEntity) {
+          const data: any | undefined = error.response?.data
+          const message = data.message || error.message
+          toast.error(message)
+        }
+        console.log(error)
+        return Promise.reject(error)
+      })
   }
 }
 
